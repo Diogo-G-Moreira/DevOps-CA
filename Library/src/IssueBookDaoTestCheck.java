2831c52ad63e5d6c1ca09a1ad598a3f764fbbc04
Diogo-G-Moreira <diogo.goncalo.moreira@gmail.com>
2017-04-18 00:12:21 +0100

    LibraryManagementSystem
    This program has been edited and unit tests has been added

---
+++ Library/src/IssueBookDaoTestCheck.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class IssueBookDaoTestCheck {
+
+	
+	@Test
+	public void IssueBookDaoTestCheck(){
+		IssueBookDao issueOp = new IssueBookDao();
+	//save(String callno,String name,String author,String publisher,int quantity)
+	boolean test;
+	assertEquals(true,test = issueOp.checkBook("A@4"));
+
+	
+
+	
+}
+	
+	
+	
+}
+
+
+
+
+
